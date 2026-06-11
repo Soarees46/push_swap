b6f4e686ef116541395b721c6d6762b6fa762b3f
Carlos Alberto Pinto Soares Ribeiro <calberto@c2r2s6.42porto.com>
2026-06-11 14:13:37 +0100

    linkedlist px

--- ft_px.c
+++ ft_px.c
@@ -6,21 +6,21 @@
 /*   By: calberto <calberto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/11 09:09:46 by vaugusto          #+#    #+#             */
-/*   Updated: 2026/06/11 11:37:35 by calberto         ###   ########.fr       */
+/*   Updated: 2026/06/11 14:12:22 by calberto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_px(int *stk_x, int *stk_y, int tam_y)
-{
-	int	tmp;
-	int	i;
+#include "push_swap.h"
 
-	if (!stk_x)
+void    ft_px(t_stack **stk_origem, t_stack **stk_destino)
+{
+	t_stack *caixa_a_mover;
+
+	if (stk_origem == NULL || *stk_origem == NULL)
 		return ;
-	i = 0;
-	while (0 < tam_y)
-	{
-		stk_y[tam_y] = stk_y[tam_y - 1];
-		tam_y--;
-	}
+
+	caixa_a_mover = *stk_origem; // PASSO 1: Agarras na primeira caixa da lista de origem com a mão para não a perder
+	*stk_origem = caixa_a_mover->next; // PASSO 2: O segundo nó da origem passa a ser o NOVO topo da lista de origem
+	caixa_a_mover->next = *stk_destino; // PASSO 3: A caixa que tens na mão vai apontar para o atual topo da lista de destino
+	*stk_destino = caixa_a_mover; // PASSO 4: A lista de destino agora passa a começar oficialmente nesta caixa nova
 }
