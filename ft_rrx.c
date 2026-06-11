5a1a9e47a6cf20b0a67cdc27a3da8115aae9d54f
Carlos Alberto Pinto Soares Ribeiro <calberto@c2r2s6.42porto.com>
2026-06-11 15:10:44 +0100

    linkedlist updated

--- ft_rrx.c
+++ ft_rrx.c
@@ -6,19 +6,28 @@
 /*   By: calberto <calberto@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/09 21:16:56 by vaugusto          #+#    #+#             */
-/*   Updated: 2026/06/11 11:37:35 by calberto         ###   ########.fr       */
+/*   Updated: 2026/06/11 15:10:20 by calberto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_rrx(int *stk, int tam)
-{
-	int	tmp;
+#include "push_swap.h"
 
-	tmp = stk[tam - 1];
-	while (tam > 0)
+void    ft_rrx(t_stack **stk)
+{
+	t_stack *penultimo;
+	t_stack *ultimo;
+
+	if (*stk == NULL || (*stk)->next == NULL)
+		return ;
+
+	penultimo = *stk; // PASSO 1: Pomos o 'penultimo' a caminhar até parar no gajo que está antes do último
+	while (penultimo->next->next != NULL)
 	{
-		stk[tam] = stk[tam -1];
-		tam--;
+		penultimo = penultimo->next; // Salta para a próxima carruagem
 	}
-	stk[0] = tmp;
+
+	ultimo = penultimo->next;// PASSO 2: O 'ultimo' vai ser a carruagem que está à frente do penúltimo
+	penultimo->next = NULL;// PASSO 3: O penúltimo passa a ser o novo fim da lista, por isso aponta para NULL
+	ultimo->next = *stk;// PASSO 4: O último gajo agora vai apontar para o antigo topo da lista
+	*stk = ultimo;// PASSO 5: A lista no main agora começa oficialmente neste 'ultimo' gajo
 }
